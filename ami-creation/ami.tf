533668a45411911f257f78969eb7cfe7c2bb0ebc
hermann90 <hermannchefouet@gmail.com>
2023-12-04 22:19:48 +0100

    Add terraform tf vars file

--- ami-creation/ami.tf
+++ ami-creation/ami.tf
@@ -6,6 +6,10 @@ provider "aws" {
 resource "aws_ami_from_instance" "ami" {
   name               = var.ami_name
   source_instance_id = var.source_id
+
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
 

14a489b7e050b3d18cc010f98fcfe14606dbab2a
Arcdarelle <162192195+Arcdarelle@users.noreply.github.com>
2025-03-06 19:44:59 +0100

    Update var.tf

--- jfrog/var.tf
+++ jfrog/var.tf
@@ -1,13 +1,32 @@
-variable aws_region {
+ariable region {
   description = "This is aws region"
-  default     = "us-west-2"
+  default     = "us-east-2"
   type        = string
 }
-
 
 variable "profile" {
   description = "user account to use"
   default = "default"
+}
+
+variable "VPC_cidr" {
+  type = string
+  default = "192.168.0.0/16" 
+}
+
+variable "public_subnet_cidr" {
+  type = string
+  default = "192.168.1.0/24"
+}  
+
+variable "AZ" {
+  type = string
+  default = "us-east-2a"
+}
+
+variable "project-name" {
+  type    = string
+  default = "jfrog"
 }
 
 # variable aws_instance_type {
